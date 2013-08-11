3943d0a9b56b88f3057c3223ce32eb93763b5d74
letusfly85 <letusfly95@gmai.com>
2013-08-11 14:02:01 +0900

    testを追加

---
+++ src/test/scala/com/jellyfish85/dbaccessor/erd/mainte/tool/dao/MsTablesDaoTest.scala
@@ -0,0 +1,28 @@
+package com.jellyfish85.dbaccessor.erd.mainte.tool.dao
+
+import org.specs2.mutable.Specification
+import com.jellyfish85.dbaccessor.erd.mainte.tool.bean.MsTablesBean
+import com.jellyfish85.dbaccessor.manager.DatabaseManager
+
+class MsTablesDaoTest extends Specification {
+
+  "return true" should {
+    val dao: MsTablesDao = new MsTablesDao
+    var bean: MsTablesBean  = new MsTablesBean
+    bean.physicalTableNameAttr.value =  "T_KK_KOKYK_KHN"
+
+    val db = new DatabaseManager
+    db.connect
+    bean = dao.find(db.conn, bean)(0)
+
+    //顧客基本テーブルを指定してエンティティが取得できることを確認する
+    "return true for T_KK_KOKYK_KHN" in {
+      val _bean: MsTablesBean = new MsTablesBean
+      _bean.physicalTableNameAttr.value = "T_KK_KOKYK_KHN"
+      _bean.logicalTableNameAttr.value  = "トラン_KK_顧客_基本"
+
+      bean.logicalTableNameAttr.value  must beEqualTo(_bean.logicalTableNameAttr.value)
+    }
+  }
+
+}
