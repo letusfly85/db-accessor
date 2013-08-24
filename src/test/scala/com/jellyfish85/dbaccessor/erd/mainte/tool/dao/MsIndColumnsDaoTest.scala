f0c0f6b6c679f5cfc97a69244b9c654a873a6888
= <=>
2013-08-24 21:49:24 +0900

    add empty MS_IND_COLUMNS test class

---
+++ src/test/scala/com/jellyfish85/dbaccessor/erd/mainte/tool/dao/MsIndColumnsDaoTest.scala
@@ -0,0 +1,21 @@
+package com.jellyfish85.dbaccessor.erd.mainte.tool.dao
+
+import org.specs2.mutable.Specification
+
+/**
+ * == MsIndColumnsDaoTest ==
+ *
+ *
+ */
+class MsIndColumnsDaoTest extends Specification {
+  //TODO insert
+
+  //TODO delete
+
+  //TODO update
+
+  //TODO select
+
+  //TODO exception
+
+}
