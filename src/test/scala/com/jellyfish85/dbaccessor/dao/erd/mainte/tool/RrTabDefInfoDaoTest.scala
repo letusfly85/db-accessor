fd7c230f6e894c44b39dcbc92e281f109816310f
= <=>
2013-08-31 11:30:25 +0900

    add update, exception test case to RrTabDefInfoDaoTest.scala

--- src/test/scala/com/jellyfish85/dbaccessor/dao/erd/mainte/tool/RrTabDefInfoDaoTest.scala
+++ src/test/scala/com/jellyfish85/dbaccessor/dao/erd/mainte/tool/RrTabDefInfoDaoTest.scala
@@ -5,6 +5,7 @@ import com.jellyfish85.dbaccessor.manager.DatabaseManager
 import com.jellyfish85.dbaccessor.bean.erd.mainte.tool.RrTabDefInfoBean
 
 import java.math.BigDecimal
+import java.sql.SQLException
 
 /**
  * == RrTabDefInfoDaoTest ==
@@ -27,18 +28,45 @@ class RrTabDefInfoDaoTest extends Specification {
     bean00.logicalColumnNameAttr.value = "SAMPLE_COLUMN"
 
     dao.delete(db.conn, bean00)
-    val result: Int = dao.insert(db.conn, List(bean00))
+    val result00: Int = dao.insert(db.conn, List(bean00))
     db.jCommit
-    "return 1 for insert result" in {
-      result must beEqualTo(1)
+    "return 1 for insert result00" in {
+      result00 must beEqualTo(1)
     }
 
     val bean01: RrTabDefInfoBean = dao.find(db.conn, bean00).head
-    db.jClose
-    "return true for RrTabDefInfoBean" in {
+    "return true for Inserted RR_TAB_DEF_INFO Record" in {
       bean01.tabDefRevisionAfAttr.value must equalTo(new BigDecimal(9999999999L))
       bean01.tabDefNameAttr.value       must equalTo("TABLE_DEFINE.xls")
     }
+
+    bean00.logicalTableNameAttr.value  = "サンプル2"
+    val result01: Int = dao.update(db.conn, List(bean00))
+    db.jCommit
+    "return 1 for insert result01" in {
+      result01 must beEqualTo(1)
+    }
+
+    val bean02: RrTabDefInfoBean = dao.find(db.conn, bean00).head
+    db.jClose
+    "return true for Updated RR_TAB_DEF_INFO Record" in {
+      bean02.tabDefRevisionAfAttr.value must equalTo(new BigDecimal(9999999999L))
+      bean02.logicalTableNameAttr.value must equalTo("サンプル2")
+    }
   }
 
+  "return error" should {
+    db.connect
+
+    val bean00: RrTabDefInfoBean = new RrTabDefInfoBean
+    bean00.tabDefIdAttr.value          = new BigDecimal(1)
+    bean00.tabDefRevisionAfAttr.value  = new BigDecimal(9999999999L)
+    bean00.tabDefNameAttr.value        = "TABLE_DEFINE.xls"
+    bean00.columnIdAttr.value          = new BigDecimal(1)
+    bean00.logicalColumnNameAttr.value = "SAMPLE_COLUMN"
+
+    dao.delete(db.conn, bean00)
+    db.jCommit
+    (dao.insert(db.conn, List(bean00))) must throwA[SQLException]
+  }
 }
