963f3caac37b26536e35658341be6098fa58c04c
letusfly85 <letusfly95@gmail.com>
2013-11-01 19:12:25 +0900

    add test

---
+++ src/test/scala/com/jellyfish85/dbaccessor/dao/src/mainte/tool/RsSvnSrcInfoDaoTest.scala
@@ -0,0 +1,11 @@
+package com.jellyfish85.dbaccessor.dao.src.mainte.tool
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: wada-s
+ * Date: 13/11/01
+ * Time: 19:12
+ */
+class RsSvnSrcInfoDaoTest {
+
+}
