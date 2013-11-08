a2c0784fc4c4cf7fa401e7ea05fcf1744e8c8d4d
letusfly85 <letusfly95@gmail.com>
2013-11-08 16:49:11 +0900

    add RS_SQL_TEXT ORMapper modules

---
+++ src/main/scala/com/jellyfish85/dbaccessor/bean/src/mainte/tool/RsSqlTextBean.scala
@@ -0,0 +1,24 @@
+package com.jellyfish85.dbaccessor.bean.src.mainte.tool
+
+import com.jellyfish85.dbaccessor.bean.{ColumnAttribute, GeneralBean}
+import java.math.BigDecimal
+
+class RsSqlTextBean extends GeneralBean {
+
+  val logicalTableName:  String =  ""
+  val physicalTableName: String =  "RS_SQL_TEXT"
+
+  var headRevisionAttr:	ColumnAttribute[BigDecimal] = new ColumnAttribute()
+  var projectNameAttr:	ColumnAttribute[String] = new ColumnAttribute()
+  var fileNameAttr:	ColumnAttribute[String] = new ColumnAttribute()
+  var pathAttr:	ColumnAttribute[String] = new ColumnAttribute()
+  var persisterNameAttr:	ColumnAttribute[String] = new ColumnAttribute()
+  var lineAttr:	ColumnAttribute[BigDecimal] = new ColumnAttribute()
+  var textAttr:	ColumnAttribute[String] = new ColumnAttribute()
+  var revisionAttr:	ColumnAttribute[BigDecimal] = new ColumnAttribute()
+  var authorAttr:	ColumnAttribute[String] = new ColumnAttribute()
+  var commitYmdAttr:	ColumnAttribute[String] = new ColumnAttribute()
+  var commitHmsAttr:	ColumnAttribute[String] = new ColumnAttribute()
+  var extensionAttr:	ColumnAttribute[String] = new ColumnAttribute()
+
+}
