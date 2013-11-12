ef592fdc462bc7add72088f442638b5feb580662
letusfly85 <letusfly95@gmail.com>
2013-11-12 10:00:47 +0900

    remove non used attributes

--- src/main/scala/com/jellyfish85/dbaccessor/bean/src/mainte/tool/RsSqlTextExpBean.scala
+++ src/main/scala/com/jellyfish85/dbaccessor/bean/src/mainte/tool/RsSqlTextExpBean.scala
@@ -10,19 +10,4 @@ class RsSqlTextExpBean extends GeneralBean with RsSqlTextBeanTrait {
 
   var subLineAttr:	ColumnAttribute[BigDecimal] = new ColumnAttribute()
 
-  /*
-  var headRevisionAttr:	ColumnAttribute[BigDecimal] = new ColumnAttribute()
-  var projectNameAttr:	ColumnAttribute[String] = new ColumnAttribute()
-  var fileNameAttr:	ColumnAttribute[String] = new ColumnAttribute()
-  var pathAttr:	ColumnAttribute[String] = new ColumnAttribute()
-  var lineAttr:	ColumnAttribute[BigDecimal] = new ColumnAttribute()
-
-  var textAttr:	ColumnAttribute[String] = new ColumnAttribute()
-  var revisionAttr:	ColumnAttribute[BigDecimal] = new ColumnAttribute()
-  var authorAttr:	ColumnAttribute[String] = new ColumnAttribute()
-  var commitYmdAttr:	ColumnAttribute[String] = new ColumnAttribute()
-  var commitHmsAttr:	ColumnAttribute[String] = new ColumnAttribute()
-  var extensionAttr:	ColumnAttribute[String] = new ColumnAttribute()
-  */
-
 }
