80d5d19994f6d86a40ad579ce61e82dfa5a9a90b
letusfly85 <letusfly95@gmail.com>
2014-02-15 13:54:58 +0900

    add delete all and insert and sql exception test case

--- src/test/scala/com/jellyfish85/dbaccessor/dao/src/mainte/tool/TrCommitHistoryDaoTest.scala
+++ src/test/scala/com/jellyfish85/dbaccessor/dao/src/mainte/tool/TrCommitHistoryDaoTest.scala
@@ -11,6 +11,7 @@ import org.dbunit.dataset.excel.XlsDataSet
 import java.io.{FileInputStream, File}
 import org.dbunit.operation.DatabaseOperation
 import com.jellyfish85.dbaccessor.bean.src.mainte.tool.TrCommitHistoryBean
+import java.sql.SQLException
 
 class TrCommitHistoryDaoTest extends Specification {
 
@@ -49,5 +50,39 @@ class TrCommitHistoryDaoTest extends Specification {
     "return true for TR_COMMIT_HISTORY" in {
       bean12.repositoryKindAttr.value must beEqualTo("subversion")
     }
+
+    val bean02: TrCommitHistoryBean = new TrCommitHistoryBean
+    bean02.repositoryKindAttr.setValue("git")
+    bean02.repositoryNameAttr.setValue("my.repository")
+    bean02.rootUrlAttr.setValue("http:://my.url2")
+    bean02.rightBaseUrlAttr.setValue("/my/trunk/src")
+    bean02.leftBaseUrlAttr.setValue("/my/branches/b01/src")
+    bean02.pathAttr.setValue("my.path2")
+
+    dao.insert(db.conn, List(bean02))
+    db.jCommit
+
+    val bean14: TrCommitHistoryBean = dao.find(db.conn, bean02).head
+    "return true for TR_COMMIT_HISTORY" in {
+      bean14.repositoryKindAttr.value must beEqualTo("git")
+      bean14.pathAttr.value           must beEqualTo("my.path2")
+    }
+
+    dao.deleteAll(db.conn)
+    db.jCommit
+    val list: List[TrCommitHistoryBean] = dao.find(db.conn, bean02)
+    "return empty for TR_COMMIT_HISTORY" in {
+      list must beEmpty
+    }
+  }
+
+  "return SQLException for TR_COMMIT_HISTORY.path IS NULL" should {
+    db.connect
+
+    val bean00: TrCommitHistoryBean    = new TrCommitHistoryBean
+    bean00.repositoryKindAttr.setValue("git")
+
+    dao.deleteAll(db.conn)
+    (dao.insert(db.conn, List(bean00))) must throwA[SQLException]
   }
 }
